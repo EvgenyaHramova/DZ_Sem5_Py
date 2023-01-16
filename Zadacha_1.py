521e095247db3d44451adfecbb272619af49402e
EvgenyaHramova <evgenya.hramova@yandex.ru>
2023-01-16 19:14:45 +0400

    new file adacha_1

---
+++ Zadacha_1.py
@@ -0,0 +1,17 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+with open('input_file.txt', 'r', encoding='utf-8') as file:
+    some_list = file.read().split()
+    print(some_list)
+
+
+new_list = list(filter(lambda word: 'а' not in word, some_list))
+new_list = list(filter(lambda word: 'б' not in word, new_list))
+new_list = ' '.join(list(filter(lambda word: 'в' not  in word, new_list)))
+
+#print()
+#print(new_list)
+
+with open('output_file.txt', 'w', encoding="utf-8") as file:
+      print(new_list, file = file)
